a6c197889fd982bdc1bb6269767292e6c7ef2d73
Jay Pfaffman <jay@literatecomputing.com>
2024-08-08 13:18:46 -0500

    push the right code

---
+++ javascripts/discourse/api-initializers/discourse-affiliate-linker.js
@@ -0,0 +1,24 @@
+import { apiInitializer } from "discourse/lib/api";
+
+export function decorateInlineLink(element) {
+  const containers = element.querySelectorAll(".cooked a");
+  containers.forEach((container) => {
+    const href = container.href;
+    console.log("fixing href", container.href);
+
+    // Check if the URL is for dronesafestore.com with or without www
+    const regex = /^https?:\/\/(www\.)?dronesafestore\.com(\/?)?$/i;
+
+    if (regex.test(href)) {
+      // Rewrite the link
+      console.log('rewriting link', settings.affiliate_link);
+      container.href = `https://dronesafestore.com/${settings.affiliate_link}`;
+    }
+  });
+}
+
+export default apiInitializer("1.8.0", (api) => {
+  api.decorateCookedElement((element) => {
+    decorateInlineLink(element);
+  });
+});
