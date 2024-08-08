f9bf5d9af12cd32ab7c4146d04aaa57bd00b2803
Jay Pfaffman <jay@literatecomputing.com>
2024-08-08 13:19:54 -0500

    remove debug loggin

--- javascripts/discourse/api-initializers/discourse-affiliate-linker.js
+++ javascripts/discourse/api-initializers/discourse-affiliate-linker.js
@@ -4,14 +4,12 @@ export function decorateInlineLink(element) {
   const containers = element.querySelectorAll(".cooked a");
   containers.forEach((container) => {
     const href = container.href;
-    console.log("fixing href", container.href);
 
     // Check if the URL is for dronesafestore.com with or without www
     const regex = /^https?:\/\/(www\.)?dronesafestore\.com(\/?)?$/i;
 
     if (regex.test(href)) {
       // Rewrite the link
-      console.log('rewriting link', settings.affiliate_link);
       container.href = `https://dronesafestore.com/${settings.affiliate_link}`;
     }
   });
